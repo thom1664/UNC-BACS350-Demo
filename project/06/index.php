e673e3b3f528f1452baf6c6332e51f9d4a417eea
Mark Seaman <mark.seaman@shrinking-world.com>
2018-09-05 17:07:55 -0600

    Build better demo for Lesson 6

--- project/06/index.php
+++ project/06/index.php
@@ -1,17 +1,22 @@
 <?php
 
+    // Start the page
+    $page_title = 'BACS 350 - Project #6';
+    require_once 'header.php';
+
+    
     // Form the DB Connection string
     $port = '3306';
     $dbname = 'uncobacs_subscribers';
-    $db_connect = "mysql:host=localhost:$port;dbname=$dbname";
     $username = 'uncobacs_350';
     $password = 'BACS_350';
-
-    echo "<h1>DB Connection</h1>" .
-        "<p>Connect String:  $db_connect, $username, $password</p>";
-
+    $db_connect = "mysql:host=localhost:$port;dbname=$dbname";
+    
 
     // Open the database or die
+    echo "<h2>DB Connection</h2>" .
+        "<p>Connect String:  $db_connect, $username, $password</p>";
+
     try {
         $db = new PDO($db_connect, $username, $password);
         echo '<p><b>Successful Connection</b></p>';
@@ -22,6 +27,7 @@
 
 
     // Query for all subscribers
+    echo "<h2>Subscribers</h2>";
     $query = "SELECT * FROM subscribers";
 
     $statement = $db->prepare($query);
@@ -35,4 +41,9 @@
         echo '<li>' . $s['name'] . ', ' . $s['email'] . '</li>';
     }
     echo '</ul>';
+
+
+    // End the page
+    require_once 'footer.php';
+
 ?>
