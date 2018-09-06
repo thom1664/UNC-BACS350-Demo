2a3e0360fae7155c189dfc21ca702dd789ed0413
Mark Seaman <Mark.Seaman@imac.net>
2018-09-06 13:52:14 -0600

    Update templates

--- project/08/todo.php
+++ project/08/todo.php
@@ -4,7 +4,7 @@
     $page_title = "Project #7 - To Do List";
 
     // Include the page start
-    include '/bacs_350/header.php';
+    include 'header.php';
 
     // Include the main page content
     echo 
@@ -29,79 +29,90 @@
 
         <pre>
 
-        Local repository
-        * Use Github Desktop to clone your BACS_350 repo
-        * File, Clone repository...
+            Clone Local repository
+            * Use Github Desktop to clone your BACS_350 repo
+            * File, Clone repository...
 
-        Pull fresh code
-        * Using the Github website pull from Mark-Seaman BACS 350 repo
-        * Confirm code on Github
-        * Use Github Desktop to pull code to local computer
-        * Confirm you have project/08 on your local computer
+            Pull fresh code
+            * Using the Github Desktop sync code from Mark-Seaman BACS 350 repo
+            * Confirm code by looking for project/08
+            * Use Github Desktop to pull code to local computer
+            * Confirm you have project/08 on your local computer
 
-        Setup Workflow
-        * Github Desktop (connect to BACS 350)
-        * FileZilla (connect to Bluehost)
-        * Windows Explorer (desktop shortcut)
-        * Chrome (local and remote bookmarks)
-        * Brackets (source code)
+            Setup Workflow
+            * Github Desktop (connect to BACS 350)
+            * FileZilla (connect to Bluehost)
+            * Windows Explorer (desktop shortcut)
+            * Chrome (local and remote bookmarks)
+            * Brackets (source code)
 
-        Simple page
-        * Use the simple.php page template to create project/08/index.php
-        * Deploy and test
-        * Add stylesheet, and images last (see what happens)
+            Fix up todo.php
+            * Deploy project/08/todo.php
+            * Test with web browser
+            * A starter file is in project/08/todo.php
+            * Add the extra files that are needed to make the stylesheet work properly
+            * Deploy and test
 
-        Stylish page
-        * User the header and footer templates to create a better page
-        * Deploy and test
+            Commit all changes
+            * Use Github Desktop to commit your changes
+            * Push to your remote repo
 
-        Validate HTML
-        * Use the HTML validator to make sure that we have good HTML
+            Simple page
+            * Use the simple.php page template to create project/08/index.php
+            * Deploy and test
+            * Add stylesheet, and images last (see what happens)
 
-        Connect to Database
-        * Use db.php template to create a database connection
-        * Place code directly into index.php
-        * Deploy & test & validate
+            Stylish page
+            * User the header and footer templates to create a better page
+            * Deploy and test
 
-        Refactor Connection
-        * Move DB connection into db.php
-        * Deploy & test & validate
+            Validate HTML
+            * Use the HTML validator to make sure that we have good HTML
 
-        Show subscribers
-        * Use select.php template to list the subscribers
-        * Deploy & test & validate
-        * Refactor into select.php
-        * Deploy & test & validate
+            Connect to Database
+            * Use db.php template to create a database connection
+            * Place code directly into index.php
+            * Deploy & test & validate
 
-        Create add view
-        * Use insert.php template to create add.php
-        * Add link to add.php on index.php
-        * Deploy & test & validate
+            Refactor Connection
+            * Move DB connection into db.php
+            * Deploy & test & validate
 
-        Build form for input
-        * Use add_form.php template to create add_form.php
-        * Set action to add.php
-        * Link to add_form.php from index.php
-        * Deploy & test & validate
+            Show subscribers
+            * Use select.php template to list the subscribers
+            * Deploy & test & validate
+            * Refactor into select.php
+            * Deploy & test & validate
 
-        Extract data from POST
-        * Modify add.php to use input from POST operation
-        * Deploy & test & validate
+            Create add view
+            * Use insert.php template to create add.php
+            * Add link to add.php on index.php
+            * Deploy & test & validate
 
-        Automatically show subscribers
-        * When add is successful go the index.php page
-        * Deploy & test & validate
+            Build form for input
+            * Use add_form.php template to create add_form.php
+            * Set action to add.php
+            * Link to add_form.php from index.php
+            * Deploy & test & validate
 
-        Commit all changes
-        * Use Github Desktop to commit your changes
-        * Push to your remote repo
+            Extract data from POST
+            * Modify add.php to use input from POST operation
+            * Deploy & test & validate
 
-        Submit your URL in Canvas
-        * Post project/08/index.php
+            Automatically show subscribers
+            * When add is successful go the index.php page
+            * Deploy & test & validate
 
-    </pre>';
+            Commit all changes
+            * Use Github Desktop to commit your changes
+            * Push to your remote repo
+
+            Submit your URL in Canvas
+            * Post project/08/index.php
+
+        </pre>';
 
     // Include the page end
-    include '/bacs_350/footer.php';
+    include 'footer.php';
 
  ?>
