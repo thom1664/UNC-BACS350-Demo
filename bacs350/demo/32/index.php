d9a9603584ea3a378dc3f59609657f2aa27f92db
Mark Seaman <Mark.Seaman@imac.net>
2019-11-07 11:12:39 -0700

    Add demo 32

---
+++ bacs350/demo/32/index.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once 'db.php';
+    require_once 'log.php';
+    require_once 'subscriber.php';
+    require_once 'views.php';
+
+
+    // Log the page load
+    log_page($db);
+
+
+    // Display the page content
+    $content .= render_button('Other Demos', '..');
+    $content .= render_button('Show Log', 'pagelog.php');
+    $content .= handle_actions();
+
+
+    // Create main part of page content
+    $settings = array(
+        "site_title" => "UNC BACS 350 Demo",
+        "page_title" => "Demo 30 - MVC Design Pattern",
+        "logo"       => "Bear.png",
+        "style"      => 'style.css',
+        "content"    => $content);
+
+    echo render_page($settings);
+
+?>
---
+++ bacs350/demo/32/pagelog.php
@@ -0,0 +1,32 @@
+<?php
+
+    require_once 'views.php';
+    require_once 'log.php';
+
+
+    // Clear the log if requested
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == 'clear') {
+        clear_log($db);
+    }
+
+
+    // Clear button
+    $home = render_button('Home', 'index.php');
+    $clear = render_button('Clear Log', 'pagelog.php?action=clear');
+
+
+    // Show page history
+    $history = render_history(query_log($db));
+
+
+    // Show Page
+    $settings = array(
+        "site_title" => "UNC BACS 350 Demo",
+        "page_title" => "Display Pages History",
+        "style"      => 'style.css',
+        "content"    => $home . $clear . $history);
+
+    echo render_page($settings);
+
+?>
